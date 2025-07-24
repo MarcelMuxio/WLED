cc1202199fa87f2ef2c20f56a34c3bae133326be
MarcelMuxio <marcel@muxio.nl>
2025-07-24 18:04:46 +0200

    Update usermod_rotaryrackdimmer.h

--- usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
+++ usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
@@ -7,32 +7,14 @@ private:
   int pinA = 33;  // A (CLK) pin van de encoder
   int pinB = 13;  // B (DT) pin van de encoder
   int lastState = 0;
+  unsigned long lastTurn = 0;
+  const unsigned long debounceDelay = 5; // ms
 
 public:
-  void setup() override {
-    pinMode(pinA, INPUT_PULLUP);
-    pinMode(pinB, INPUT_PULLUP);
-    lastState = digitalRead(pinA);
-  }
-
-  void loop() override {
-    int currentState = digitalRead(pinA);
-    if (currentState != lastState) {
-      if (digitalRead(pinB) != currentState) {
-        // Rechtsom → dim omhoog
-        bri = min(255, bri + 5);
-      } else {
-        // Linksom → dim omlaag
-        bri = max(0, bri - 5);
-      }
-      lastState = currentState;
-      colorUpdated(CALL_MODE_DIRECT_CHANGE);
-    }
-  }
-
+  void setup() override;
+  void loop() override;
   void addToConfig(JsonObject &root) override {}
   bool readFromConfig(JsonObject &root) override { return true; }
-
+  void addToJsonInfo(JsonObject &root) override;
   String getId() override { return F("RotaryRackDimmer"); }
 };
-
