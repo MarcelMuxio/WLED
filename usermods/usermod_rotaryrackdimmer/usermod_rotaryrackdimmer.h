1284ca0385c5cbbbdec7286c47e85ac134f98189
MarcelMuxio <marcel@muxio.nl>
2025-07-27 11:36:45 +0200

    Update usermod_rotaryrackdimmer.h

--- usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
+++ usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
@@ -1,27 +1,21 @@
 #pragma once
-
 #include "wled.h"
 
 class Usermod_RotaryRackDimmer : public Usermod {
-  private:
-    bool initDone = false;
+private:
+  int pinA = 12;
+  int pinB = 13;
+  int pinButton = 14;
+  int lastState = HIGH;
+  bool initDone = false;
+  unsigned long lastTurn = 0;
+  const unsigned long debounceDelay = 50;
+  bool colorMode = false;  // false = dim, true = color
+  bool lastButtonState = HIGH;
 
-    // GPIO pinnen voor rotary encoder en knop
-    const uint8_t pinA = 32;       // Pas aan indien nodig
-    const uint8_t pinB = 33;       // Pas aan indien nodig
-    const uint8_t pinButton = 25;  // Drukknop pin
-
-    // Voor encoder en knop
-    int lastState = HIGH;
-    unsigned long lastTurn = 0;
-    const unsigned long debounceDelay = 50;
-
-    bool lastButtonState = HIGH;
-    bool colorMode = false;  // false = dim, true = kleurmodus
-
-  public:
-    void setup() override;
-    void loop() override;
-    void addToJsonInfo(JsonObject& root) override;
-    uint16_t getId() override;
+public:
+  void setup() override;
+  void loop() override;
+  void addToJsonInfo(JsonObject& root) override;
+  uint16_t getId() override;
 };
