70b182abf66e2d7330d016224faa3e6d79d59e38
MarcelMuxio <marcel@muxio.nl>
2025-07-27 09:14:37 +0200

    Update usermod_rotaryrackdimmer.h

--- usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
+++ usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
@@ -1,18 +1,30 @@
 #pragma once
+
 #include "wled.h"
 
 class Usermod_RotaryRackDimmer : public Usermod {
-  private:
-    int pinA = 33;
-    int pinB = 13;
-    int lastState = 0;
-    unsigned long lastTurn = 0;
-    const unsigned long debounceDelay = 50;
-    bool initDone = false;
+private:
+  int pinA = 33;          // Rotary encoder CLK
+  int pinB = 13;          // Rotary encoder DT
+  int pinButton = 12;     // Drukknop van rotary encoder
+  int lastState = 0;
+  bool isColorMode = false;
+  bool lastButtonState = true;
+  unsigned long lastButtonPress = 0;
+  const unsigned long debounceDelay = 200;
+  unsigned long lastTurn = 0;
 
-  public:
-    void setup() override;
-    void loop() override;
-    void addToJsonInfo(JsonObject &root) override;
-    uint16_t getId() override;
+  uint8_t colorIndex = 0;
+  CRGB presetColors[2] = {
+    CRGB(255, 0, 0), // Wit
+    CRGB(0, 255, 0)      // Blauw
+  };
+
+  bool initDone = false;
+
+public:
+  void setup() override;
+  void loop() override;
+  void addToJsonInfo(JsonObject &root) override;
+  uint16_t getId() override;
 };
