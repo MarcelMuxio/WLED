578851b278663071f1609bd0474fcc88b4b316a9
MarcelMuxio <marcel@muxio.nl>
2025-07-27 09:15:20 +0200

    Update usermod_rotaryrackdimmer.cpp

--- usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.cpp
+++ usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.cpp
@@ -3,25 +3,51 @@
 void Usermod_RotaryRackDimmer::setup() {
   pinMode(pinA, INPUT_PULLUP);
   pinMode(pinB, INPUT_PULLUP);
+  pinMode(pinButton, INPUT_PULLUP);
+
   lastState = digitalRead(pinA);
+  lastButtonState = digitalRead(pinButton);
+
   initDone = true;
 }
 
 void Usermod_RotaryRackDimmer::loop() {
   if (!initDone || strip.isUpdating()) return;
 
-  if (millis() - lastTurn < debounceDelay) return;
+  // Rotary draai (dim of kleurwissel)
+  if (millis() - lastTurn > debounceDelay) {
+    int currentState = digitalRead(pinA);
+    if (currentState != lastState) {
+      lastTurn = millis();
+      bool dir = digitalRead(pinB) != currentState;
 
-  int currentState = digitalRead(pinA);
-  if (currentState != lastState) {
-    lastTurn = millis();
-    if (digitalRead(pinB) != currentState) {
-      bri = min(255, bri + 5); // Rechtsom
-    } else {
-      bri = max(0, bri - 5);   // Linksom
+      if (isColorMode) {
+        // Wissel tussen wit/blauw
+        colorIndex = 1 - colorIndex;
+        CRGB newColor = presetColors[colorIndex];
+        setColor(0, RGBW32(newColor.r, newColor.g, newColor.b, 0));
+      } else {
+        // Dimmodus
+        if (dir) {
+          bri = min(255, bri + 15);
+        } else {
+          bri = max(0, bri - 15);
+        }
+        colorUpdated(CALL_MODE_DIRECT_CHANGE);
+      }
+
+      lastState = currentState;
     }
-    lastState = currentState;
-    colorUpdated(CALL_MODE_DIRECT_CHANGE);
+  }
+
+  // Drukknop detecteren
+  bool currentButtonState = digitalRead(pinButton);
+  if (lastButtonState != currentButtonState) {
+    lastButtonState = currentButtonState;
+    if (!currentButtonState && (millis() - lastButtonPress > debounceDelay)) {
+      isColorMode = !isColorMode;
+      lastButtonPress = millis();
+    }
   }
 }
 
@@ -31,17 +57,16 @@ void Usermod_RotaryRackDimmer::addToJsonInfo(JsonObject &root) {
 
   JsonObject mod = user.createNestedObject("RotaryRackDimmer");
   mod["Brightness"] = bri;
+  mod["Mode"] = isColorMode ? "Color" : "Dim";
 }
 
 #ifndef USERMOD_ID_ROTARYRACKDIMMER
 #define USERMOD_ID_ROTARYRACKDIMMER 2501
 #endif
 
-
 uint16_t Usermod_RotaryRackDimmer::getId() {
-  return USERMOD_ID_ROTARYRACKDIMMER; // Dit moet in const.h gedefinieerd worden
+  return USERMOD_ID_ROTARYRACKDIMMER;
 }
 
 static Usermod_RotaryRackDimmer rotaryMod;
 REGISTER_USERMOD(rotaryMod);
-
