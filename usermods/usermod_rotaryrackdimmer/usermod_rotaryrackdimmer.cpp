737d7491b0bc45a6d8411834ee6593936802ef52
MarcelMuxio <marcel@muxio.nl>
2025-08-05 09:28:56 +0200

    Update usermod_rotaryrackdimmer.cpp

--- usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.cpp
+++ usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.cpp
@@ -1,6 +1,5 @@
 #include "usermod_rotaryrackdimmer.h"
 
-// Setup the usermod
 void Usermod_RotaryRackDimmer::setup() {
   pinMode(pinA, INPUT_PULLUP);
   pinMode(pinB, INPUT_PULLUP);
@@ -8,7 +7,7 @@ void Usermod_RotaryRackDimmer::setup() {
   lastState = digitalRead(pinA);
   lastButtonState = digitalRead(pinButton);
 
-  // Init kleur naar blauw
+  // Startkleur blauw
   col[0] = RGBW32(0, 0, 255, 0);
   colorBlend = 0.0f;
 
@@ -18,7 +17,7 @@ void Usermod_RotaryRackDimmer::setup() {
 void Usermod_RotaryRackDimmer::loop() {
   if (!initDone || strip.isUpdating()) return;
 
-  // Rotary draaien (debounce check)
+  // Rotary encoder draaien
   if (millis() - lastTurn > debounceDelay) {
     int currentState = digitalRead(pinA);
     if (currentState != lastState) {
@@ -26,24 +25,24 @@ void Usermod_RotaryRackDimmer::loop() {
       bool dir = digitalRead(pinB) != currentState;
 
       if (colorMode) {
-        // Kleur overgangen: links = witter, rechts = blauwer
+        // Langzaam overgaan tussen blauw (rechts) en wit (links)
         colorBlend = constrain(colorBlend + (dir ? -0.05f : 0.05f), 0.0f, 1.0f);
-
-        uint8_t r = colorBlend * 255;
-        uint8_t g = colorBlend * 255;
-        uint8_t b = 255;
-
+        byte r = colorBlend * 255;
+        byte g = colorBlend * 255;
+        byte b = 255 - (colorBlend * 255);
         col[0] = RGBW32(r, g, b, 0);
+        colorUpdated(CALL_MODE_DIRECT_CHANGE);
       } else {
-        // Dimmer: rechtsom = feller (omgedraaid zoals gevraagd)
+        // Dimmen (rechtsom = feller)
         bri = constrain(bri + (dir ? 15 : -15), 0, 255);
+        colorUpdated(CALL_MODE_DIRECT_CHANGE);
       }
-      colorUpdated(CALL_MODE_DIRECT_CHANGE);
+
       lastState = currentState;
     }
   }
 
-  // Drukknop check
+  // Drukknop voor modus wisselen
   bool currentButtonState = digitalRead(pinButton);
   if (currentButtonState != lastButtonState && currentButtonState == LOW) {
     colorMode = !colorMode;
@@ -51,7 +50,7 @@ void Usermod_RotaryRackDimmer::loop() {
   lastButtonState = currentButtonState;
 }
 
-void Usermod_RotaryRackDimmer::addToJsonInfo(JsonObject &root) {
+void Usermod_RotaryRackDimmer::addToJsonInfo(JsonObject& root) {
   JsonObject user = root["u"];
   if (!user) user = root.createNestedObject("u");
 
@@ -69,6 +68,5 @@ uint16_t Usermod_RotaryRackDimmer::getId() {
   return USERMOD_ID_ROTARYRACKDIMMER;
 }
 
-// Registreer usermod
 static Usermod_RotaryRackDimmer rotaryMod;
 REGISTER_USERMOD(rotaryMod);
